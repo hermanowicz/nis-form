745ca435c9b987aab97f406c27a36f6dca03fbba
Tomasz Hermanowicz <hermangti@gmail.com>
2025-02-24 15:58:10 +0100

    nit of new nis 2 form app old went to garbage

---
+++ NIS_II.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+st.write("""
+    # NIS II to form powered by AI Agents
+
+    This is a demo, please keep this in mind.
+    Allways consult licensed profesional.
+    Page is provided as is: we are not responsible for your decisions.
+""")
+
+st.write("""
+    Form bellow will help you answear question are you obligated to implemet NIS II directive.
+
+    - We will ask for yours or yours company website adress in form of https://your-domain.com
+
+    - Short discription of what you are doing,
+
+    This information will be feed to Ai Agent team that will compare it with NIS II document body and Polish regulation caled
+
+    Ustawa o cyberbezpieczeństwie. Ai will need some time, soo don't wory if it will take a minute, if somthing will go wrong we will
+   desplay message.
+
+  Non of the data is permanentli stored, every day system is restarted at 3 AM to clear all logs, and requests.
+""")
---
+++ pages/Technology_Stack.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+
+st.title("Technology Stack")
+
+st.header("Tooles used to build this demo app.")
+st.subheader("https://github.com/hermanowicz/nis-form")
+
+st.write("""
+    ## Software
+
+     - Fedora 41
+     - Python 3.12
+     - Agno
+     - Agno Tools
+     - Streamlit
+     - LanceDB
+     - PyPdf
+     - Ollama
+     - Cohere
+     - DuckDuckGo
+     - Loguru
+    """)
+
+st.divider()
+
+st.write("""
+    ## Hardware
+
+    - Cloud Provider -> Hetzner
+    - Instance -> 4 cpu / 8 gb / no gpu
+""")
