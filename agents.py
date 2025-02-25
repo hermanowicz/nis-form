dba3886b286f296c336f5173f9a568c9a842862d
Tomasz Hermanowicz <hermangti@gmail.com>
2025-02-25 17:04:09 +0100

    switch from local models (Phi4 was promising but it does not support toolin)g, to OpenAi for now.

--- agents.py
+++ agents.py
@@ -1,9 +1,10 @@
 from agno.agent import Agent
-from agno.models.ollama import Ollama
-# from agno.models.openai import OpenAIChat
+from agno.models.groq import Groq
+from agno.models.openai import OpenAIChat
 from agno.knowledge.pdf import PDFKnowledgeBase, PDFReader
 from agno.vectordb.lancedb import LanceDb
 from agno.vectordb.search import SearchType
+import os
 
 vector_db = LanceDb(
     table_name="regulatory_docs",
@@ -13,24 +14,22 @@ vector_db = LanceDb(
 
 knowledge_base = PDFKnowledgeBase(
     path="pdf_docs/",
-    # urls=["./pdf_docs/ustawa_o_cyberbezpieczenstwie.pdf",
-    #       "./pdf_docs/CELEX_2022_L2555_Text.pdf", "./pdf_docs/DPA_May_10_2018.pdf",],
     vector_db=vector_db,
     reader=PDFReader(chunk=True),
 )
 
 agent = Agent(
     # to be added ollama binding
-    model=Ollama(id="command-r7b:latest", host="localhost:11434"),
+    model=OpenAIChat(id="gpt-4o", temperature=0.2, max_retries=3, api_key=os.getenv("OPEN_API_KEY")),
     knowledge=knowledge_base,
-    search_knowledge=True,
     debug_mode=True,
+    search_knowledge=True,
     show_tool_calls=True,
-    description="""You are proffesional legal advisor, with knowlage about EU and Ploish regulations About NIS 2 AND GDPR (RODO), always provide clear answear with links and document paragrafs from knowledge base to suppoert your answears. Never Lie and if you are not sure tell that and redirect user to lawyer."""
+    description="""You are proffesional legal advisor, with knowlage about EU and Ploish regulations About NIS 2 AND GDPR (RODO), always provide clear answear with links and document paragrafs from knowledge base which is required to suppoert your answears. Never Lie and if you are not sure tell that and redirect user to lawyer. Always try to responde in user language."""
 )
 
 if agent.knowledge is not None:
-    agent.knowledge.load(upsert=True)
+    agent.knowledge.load()
 
 agent.print_response(
-    "Will I need implement NIS 2 as small buisnes providing server administration services to SMB in my city?", markdown=True)
+    "czy musze implementować NIS 2, jako mała firma w Warszawie zajmujaca sie web developmentem i administaracja sklepami online dla klientów z sektora SMB?", markdown=True, stream=True)
